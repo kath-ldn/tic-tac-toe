630761973e2b319ffa9d6d955e39ae71a8760a4e
Kath <katharineturner3@gmail.com>
2021-04-16 16:03:13 +0100

    V3.0 - Updated AI & Interface

--- README.md
+++ README.md
@@ -1,25 +1,63 @@
-V.2 I have completed the minimax function using the Free Code Camp tutorial here: https://www.youtube.com/watch?v=P2TcQ3h0ipQ&ab_channel=freeCodeCamp.org .
-You can now choose between playing an easy AI or hard AI.
+# V3.0 Tic Tac Toe with Unbeatable AI
 
-For the next version I will:
--Improve visuals
--Edit the modal form validation
--Create 'medium' AI
--Test & fix bugs - e.g. if close modal without submitting info, can still play
-    -stop user being able to click before computer has
--make the turnclick function less hellish to read
--simplify easy function
+## About
 
+This is V3.0 of a Tic Tac Toe game that I made for [The Odin Project: Javascript Module, Project: Tic Tac Toe](https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/tic-tac-toe). The purpose of this was to practice organizing Javascript, factory functions and module patterns. The main objective was to have as little global code as possible. There was an additional option to create an unbeatable AI using the minimax algorithm (which I added in versions 2 and 3).
 
-V.1 of Tic Tac Toe.
+### Project History
 
-I have included 2 JS files - one which I'm happy with (aside from styles) where the computer makes a random play. I'm sure it could be streamlined but it functions.
+* V1.0: Basic Tic Tac Toe
+* V2.0: Above, with unbeatable AI using [Free Code Camp tutorial](https://www.youtube.com/watch?v=P2TcQ3h0ipQ&ab_channel=freeCodeCamp.org)
+* V3.0:
+    * Refactored code
+    * Re-wrote unbeatable AI
+    * Refined UI/UX
+    * Added basic mobile responsiveness (not widely tested)
+    * Added 'medium' setting
 
-The AI has been TRICKY. I've been working on this for a few days now. For about a day i was stuck with it making all possible moves, and now I've got it to a point where it is making a logical but not optimal move, and I can't work out the logic. I am going to move on and come back to it.
+The V3.0 unbeatable AI is still based on the FCC tutorial, but I re-wrote it to suit the rest of my gameplay, and to further research and practice recursion. It's probably not as slick as the FCC one, but I understand it and have worked through it and modified line-by-line - which feels more important!
 
-to fix
--invoking red fumction
--enter own name
--reset + play again
--function endgame
--ctrl f tie
+### Live Demo
+
+[GitHub Pages](https://kath-ldn.github.io/tic-tac-toe/).
+
+### Built with
+
+* HTML
+* CSS
+* Javascript
+
+## Prerequisites & Installation
+
+No special requirements.
+
+## Usage
+
+Play Tic Tac Toe against the computer - choosing your name, symbol and difficulty setting.
+
+## Contributing
+
+For any significant changes - please open an issue first.
+
+## Licence
+
+Copyright (c) 2021 Katharine Turner
+
+## Roadmap
+
+* Address the known issues below.
+
+### Known Issues
+
+* Code could be more simple, modular and concise - once I've had some space from it I will revisit.
+* Check modal form validation
+* Not tested on mobile
+* If user doesn't wait for AI - can click
+
+## Acknowledgements
+
+* [Free Code Camp JS/Tic Tac Toe unbeatable AI tutorial](https://www.youtube.com/watch?v=P2TcQ3h0ipQ&ab_channel=freeCodeCamp.org)
+* Google Fonts
+* Meyer-Web CSS Reset
+* GitHub Pages
+* The Odin Project
--- script.js
+++ script.js
@@ -1,98 +1,109 @@
-/* SCRIPT TO CONTROL THE MODAL */
-// Get the modal
-var modal = document.getElementById("myForm");
+/* Global variables */
+const messageBoard = document.getElementById('message-board');
+let modal = document.getElementById("game-form");
+let mode = "";
+let gameBoard = [];
 
-// Get the <span> element that closes the modal
-var cancelButton = document.getElementById("cancelButton");
-
-// When the user clicks on <span> (x), close the modal
-cancelButton.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-//submit data and close modal
-function getUserInfo(form){
-  var humanName = userForm.userName.value;
-  var humanSymbol = userForm.symbol.value;
-  var aiSymbol;
-   if (humanSymbol == "O") {
-    aiSymbol = "X";
-   } else { aiSymbol = "O" }
-  event.preventDefault();
-  modal.style.display = "none";
-  human.name = humanName;
-  human.symbol = humanSymbol;
-  ai.name = "AI";
-  ai.symbol = aiSymbol;
-  displayPlayers();
-  return mode = userForm.difficulty.value;
+/* Sets up message board & variables containing messages */
+let introMessage = "Hey! =] Press 'Start' to begin...";
+let startMessage = "Great! Now click on a square to place your mark.";
+let playAgainMessage = "Play again, or press 'Start' to change your settings."
+function updateMessage(message){
+    messageBoard.textContent = message;
 };
+updateMessage(introMessage);
 
-// get modal submit button
-const submitButton = document.getElementById("submitButton");
-submitButton.addEventListener('click', getUserInfo);
-
-/* SCRIPT TO SET UP & DISPLAY PLAYERS */
-// factory function to create players
+/* Factory function to create players & player variables */
 const CreatePlayer = (name, symbol) => {
-  return {name, symbol};
-};
-
-let human = CreatePlayer("", "X");
+    return {name, symbol};
+  };
+let user = CreatePlayer("", "X");
 let ai = CreatePlayer("", "O");
 
-// function to display players on grid
-function displayPlayers() {
-  const humanNameBox = document.getElementById('userNameBox').textContent = human.name;
-      const humanSymbolBox = document.getElementById('userSymbolBox').textContent = human.symbol;
-          const aiNameBox = document.getElementById('compNameBox').textContent = ai.name;
-              const aiSymbolBox = document.getElementById('compSymbolBox').textContent = ai.symbol;
-};
-
-/* SCRIPT TO CONTROL STARTING AND RESETTING*/
-//Start button
-const startButton = document.getElementById('startButton');
-startButton.addEventListener('click', startGame);
-startButton.onclick = function() {
-  modal.style.display = "block";
+/* Gets user info from form */
+function getUserInfo(){
+    user.symbol = userForm.symbol.value;
+    ai.symbol = (user.symbol === "O") ? "X" : "O";
+    user.name = userForm.userName.value;
+    ai.name = "AI";
+    mode = userForm.difficulty.value;
 }
 
-const playAgain = document.getElementById('playAgain');
-playAgain.addEventListener('click', () => {    
-  resetBoard();
-});
+/* Sets up game once user info submitted */
+function setUpGame(event){
+    event.preventDefault();
+    getUserInfo();
+    toggleModal();
+    displayPlayers();
+    updateMessage(startMessage);
+    startGame();
+};
 
+/* Creates gameboard array */
+function createGameBoard(){
+    gameBoard = [];
+    for(let i = 0; i < 9; i++){
+        let str = 'squ' + i;
+        gameBoard.push(str);
+    }
+};
+
+/* Displays modal form for user info */
+function toggleModal(){
+    (modal.style.display === "none") ? modal.style.display = "block" : modal.style.display = "none";
+};
+
+/* IIFE to set up hidden modal */
+(function setUpModal(){
+    let cancelButton = document.getElementById("cancel-button");
+    cancelButton.onclick = () => {
+        toggleModal();
+    }
+    window.onclick = (event) => {
+        if (event.target == modal) {
+            toggleModal();
+        }
+    }
+    let userForm = document.getElementById("userForm").addEventListener('submit', (event) => {
+        setUpGame(event);
+    });
+})();
+
+/* shows player container and data once modal submitted */
+function showPlayersGrid(){
+    let playersGrid = document.getElementById("players-grid");
+    playersGrid.style.display = "grid";
+};
+function displayPlayers() {
+    const userNameContainer = document.getElementById('user-name').textContent = user.name;
+    const userSymbolContainer = document.getElementById('user-symbol').textContent = user.symbol;
+    const aiNameContainer = document.getElementById('ai-name').textContent = ai.name;
+    const aiSymbolContainer = document.getElementById('ai-symbol').textContent = ai.symbol;
+    showPlayersGrid();
+};
+
+/* Sets up nav (start, play again) */
+(function createNav(){
+    const startButton = document.getElementById('start-button');
+    startButton.addEventListener('click', toggleModal);
+})();
+
+function triggerPlayAgain(){
+    const playAgain = document.getElementById('play-again');
+    playAgain.addEventListener('click', () => {    
+    resetBoard();
+    });
+    playAgain.classList.remove("disabled-btn");
+};
+
+/* Resets board for play again */
 function resetBoard() {
   startGame();
-  messageBoard.textContent = "Click a square to begin";
+  updateMessage(playAgainMessage);
 };
 
-function resetPlayers() {
-  human.name = "";
-  ai.name = "";
-  human.symbol = "";
-  ai.symbol = "";
-  displayPlayers();
-}
-
-const resetButton = document.getElementById('resetButton');
-resetButton.addEventListener('click', () => {    
-  resetBoard();
-  resetPlayers();  
-  messageBoard.textContent = "Press 'Start' to begin";  
-});
-
-// *** SCRIPT TO CONTROL GAMEPLAY ***
-let mode;
-
-let origBoard;
+// *** Controls Gameplay ***
+//indexes of wins
 const winCombos = [
 	[0, 1, 2],
 	[3, 4, 5],
@@ -102,189 +113,201 @@ const winCombos = [
 	[2, 5, 8],
 	[0, 4, 8],
 	[6, 4, 2]
-]
+];
 
-/// variable to hold messageBoard
-const messageBoard = document.getElementById('messageBoard');
-messageBoard.textContent = "Press 'Start' To Begin";  
-//function to create winner message
-function messageWin(message) {
-       messageBoard.textContent = message; 
-       //could add a display-block here
-}
-
-// Module pattern that creates board by creating & appending buttons (with classes and ids) to parent container
-const createGameBoard = (() => {
-  const gameContainer = document.getElementById("gameContainer");
-      for (i = 0; i < 9; i++) {
+/* IIFE to creates board visuals */
+(function createGameBoard(){
+    const gameContainer = document.getElementById("game-container");
+    for (i = 0; i < 9; i++) {
         squ = document.createElement('div');
-          squ.classList.add('squ');
-            squ.setAttribute('id', i);
-                gameContainer.appendChild(squ); 
+        squ.classList.add('squ');
+        squ.setAttribute('id', 'squ' + i);
+        gameContainer.appendChild(squ); 
       }
-}
-)();
+})();
 
+/* Global variable to hold all dom elements of board */
 const squares = document.querySelectorAll('.squ');
 
-function startGame(){
-  //document.querySelector(".endgame").style.display = "none";
-  origBoard = Array.from(Array(9).keys());
-  for (let i=0; i < squares.length; i++) {
-    squares[i].textContent = '';
-    squares[i].style.color = "black";
-    squares[i].addEventListener('click', turnClick, false);
-  }
-}
-
-function turnClick(square) {
-  if (typeof origBoard[square.target.id] == 'number') {
-    turn(square.target.id, human.symbol)
-    if (!checkWin(origBoard, human.symbol) && !checkTie()) {
-        if (mode == "hard") {
-          setTimeout(function() { 
-          turn(bestSpot(), ai.symbol)
-        }, 500);
-        } else {
-          setTimeout(function() { 
-          turn(easySpot(), ai.symbol)
-        }, 500);
-}
-  }
-}
-}
-
-//
-// TO ADD function for computer 'easy' (random) play
-//function for computer play
-function easySpot() {
-  let randomNumber = Math.random();
-    if (randomNumber < 0.11 && origBoard[0] != 'O' && origBoard[0] != 'X') {
-      return 0;
-    } if (randomNumber < 0.22 && origBoard[1] != 'O' && origBoard[1] != 'X') {
-      return 1;
-    } if (randomNumber < 0.33 && origBoard[2] != 'O' && origBoard[2] != 'X') {
-      return 2;
-    } if (randomNumber < 0.44 && origBoard[3] != 'O' && origBoard[3] != 'X') {
-      return 3;
-    } if (randomNumber < 0.55 && origBoard[4] != 'O' && origBoard[4] != 'X') {
-      return 4;
-    } if (randomNumber < 0.66 && origBoard[5] != 'O' && origBoard[5] != 'X') {
-      return 5;
-    } if (randomNumber < 0.77 && origBoard[6] != 'O' && origBoard[6] != 'X') {
-      return 6;
-    } if (randomNumber < 0.88 && origBoard[7] != 'O' && origBoard[7] != 'X') {
-      return 7;
-    } if (randomNumber < 0.99 && origBoard[8] != 'O' && origBoard[8] != 'X') {
-      return 8;
+/* Controls AI position in 'medium' mode */
+function mediumPlay(){
+    let rand = Math.floor((Math.random() * 10) + 1);
+    let play;
+    if(rand <= 5){
+        play = easyPlay();
     } else {
-      let availSpots = emptySquares();
-      console.log(availSpots);
-      return availSpots[0];
-      }
-}
-
-function turn(squareID, player) {
-    origBoard[squareID] = player;
-    document.getElementById(squareID).textContent = player;
-    let gameWon = checkWin(origBoard, player);
-    if (gameWon) gameOver(gameWon);
-}
-
-//once have it working - see if can edit this to check understanding
-function checkWin(board, player) {
-  let plays = board.reduce((a, e, i) =>
-    (e === player) ? a.concat(i) : a, []);
-  let gameWon = null;
-  for (let [index, win] of winCombos.entries()) {
-    if (win.every(elem => plays.indexOf(elem) > -1)) {
-      gameWon = {index: index, player: player};
-      break;
+        play = bestSpot();
     }
-  }
-  return gameWon;
-}
+    return play;
+};
 
-function gameOver(gameWon) {
-  for (let index of winCombos[gameWon.index]) {
-    document.getElementById(index).style.color =
-      gameWon.player == human.symbol ? "blue" : "red";
-  }
-  for (let i = 0; i < squares.length; i++) {
-    squares[i].removeEventListener('click', turnClick, false);
-  }
-  messageWin(gameWon.player == human.symbol ? "You win!" : "You Lose.");
-}
-
-//look into the below - not sure I totally understand how this works
-function emptySquares() {
-  return origBoard.filter(s => typeof s == "number");
-}
-
-function bestSpot() {
-  return minimax(origBoard, ai.symbol).index;
-}
-
-function checkTie() {
-  if (emptySquares().length == 0) {
-    for (let i = 0; i < squares.length; i++) {
-      //squares[i].style.backgroundColor = "green";
-      squares[i].removeEventListener("click", turnClick, false);
-    }
-    messageWin("Tie Game!")
-    return true;
-  }
-  return false;
-}
-
-function minimax(newBoard, player) {
-  let availSpots = emptySquares();
-
-  if (checkWin(newBoard, human.symbol)) {
-    return {score: -10};
-  } else if (checkWin(newBoard, ai.symbol)) {
-    return {score: 10};
-  } else if (availSpots.length === 0) {
-    return {score: 0};
-  }
- 
-  let moves = [];
-  for (let i = 0; i <availSpots.length; i++) {
-    let move = {};
-    move.index = newBoard[availSpots[i]];
-    newBoard[availSpots[i]] = player;
-
-    if (player == ai.symbol) {
-        let result = minimax(newBoard, human.symbol);
-        move.score = result.score;
-    } else {
-        let result = minimax(newBoard, ai.symbol);
-        move.score = result.score
-    }
-
-    newBoard[availSpots[i]] = move.index;
-
-    moves.push(move);
-  }
-
-  let bestMove;
-  if(player === ai.symbol) {
-    let bestScore = -10000;
-      for (let i = 0; i < moves.length; i++) {
-        if (moves[i].score > bestScore) {
-          bestScore = moves[i].score;
-          bestMove = i;
+/* Controls user turn once squ clicked, checks for win and triggers ai turn */
+function userTurn(event){
+    let i = gameBoard.indexOf(event.target.id);
+    gameBoard[i] = user.symbol;
+    (event.target).textContent = user.symbol;
+    let win;
+    win = checkWin(gameBoard, user.symbol);
+    let tie = checkTie();
+    if(win === null && tie === false){
+        if(mode === "easy"){
+            setTimeout( () => {
+                aiTurn(easyPlay());
+            }, 500);
+        } else if(mode === "medium"){
+            let play = mediumPlay();
+            setTimeout( () => {
+            aiTurn(play);
+            }, 500); 
+        } else if(mode === "hard"){
+            let play = bestSpot();
+            setTimeout( () => {
+            aiTurn(play);
+            }, 500); 
         }
-      }
-  } else {
-    let bestScore = 10000;
-    for(let i = 0; i < moves.length; i++) {
-      if (moves[i].score < bestScore) {
-        bestScore = moves[i].score;
-        bestMove = i;
-      }
+    }   
+    if(win != null & tie === false){
+        gameOver(win);
     }
-  }
+    if(tie === true){
+        gameOver("tie");
+    }
+};
 
-  return moves[bestMove];
-}
+/* Sets up text content, event listeners and colors for game board */
+function startGame(){
+    for (let i=0; i < squares.length; i++) {
+        squares[i].textContent = '';
+        squares[i].addEventListener('click', userTurn);
+        squares[i].style.color = "rgb(77, 116, 119)";
+    }
+    createGameBoard();
+};
+
+/* Checks for tie after user/ai play */
+function checkTie() {
+    return (emptySquares().length === 0) ? true : false;
+};
+
+/* Checks for win after user/ai play */
+function checkWin(board, player){
+    let hasWon = null;
+    for(let i=0; i < winCombos.length; i++){
+            let plays = 0;
+        for(let j=0; j < 3; j++){
+            if(gameBoard[winCombos[i][j]] === player){
+                plays++;
+            };
+            if(plays === 3){
+                hasWon = {
+                    index: winCombos[i],
+                    player: player
+                }
+            }
+        }
+    }
+    return hasWon;
+};
+
+/* Controls AI tie */
+function aiTurn(play){
+    gameBoard[gameBoard.indexOf(play)] = ai.symbol;
+    let squ = document.getElementById(play);
+    squ.textContent = ai.symbol;
+    let win = checkWin(gameBoard, ai.symbol);
+    if(win != null){
+        gameOver(win);
+    }
+};
+
+/* Determines which board squares are still open */
+function emptySquares() {
+    let emptys = [];
+    for(let i = 0; i < gameBoard.length; i++){
+        if(gameBoard[i] !== "X" && gameBoard[i] !== "O"){
+            emptys.push(gameBoard[i]);
+        }
+    }
+    return emptys;
+};
+
+/* Controls AI position in easy play */
+function easyPlay(){
+    let emptys = emptySquares();
+    let play = emptys[Math.floor(Math.random() * emptys.length)];
+    return play;
+};
+
+/* Announces winner/tie and removes events */
+function gameOver(winner) {
+    if(winner !== "tie"){
+        for(let index of winner.index){
+            let squ = document.getElementById("squ" + index);
+            squ.style.color = "#d65238";
+        }
+        updateMessage(winner.player == user.symbol ? "You win!" : "You Lose.");
+    } else {
+        updateMessage("It's a tie!!")
+    }
+    for(let i = 0; i < squares.length; i++){
+        squares[i].removeEventListener('click', userTurn);
+    }
+    triggerPlayAgain();
+};
+
+/* calls recursive minimax function and determine most optimal ai move */
+function bestSpot() {
+    let best = minimax(gameBoard, ai.symbol);
+    best = "squ" + best.index;
+    return best;
+};
+
+/* recursive minimax function and determine most optimal ai move */
+function minimax(newBoard, player) {
+    let availSpots = emptySquares();
+  
+    if (checkWin(newBoard, user.symbol)) {
+        return {score: -10};
+    } else if (checkWin(newBoard, ai.symbol)) {
+        return {score: 10};
+    } else if (availSpots.length === 0) {
+        return {score: 0};
+    }
+    
+    let moves = [];
+    for (let i = 0; i < availSpots.length; i++) {
+        let move = {};
+        let j = newBoard.indexOf(availSpots[i]);
+        move.index = j;
+        newBoard[j] = player;
+        if (player == ai.symbol) {
+            let result = minimax(newBoard, user.symbol);
+            move.score = result.score;
+        } else {
+            let result = minimax(newBoard, ai.symbol);
+            move.score = result.score;
+        }
+        newBoard[j] = "squ" + j;
+        moves.push(move);
+    }
+  
+    let bestMove;
+    if(player === ai.symbol) {
+        let bestScore = -10000;
+        for (let i = 0; i < moves.length; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    } else {
+        let bestScore = 10000;
+        for(let i = 0; i < moves.length; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    }
+    return moves[bestMove];
+};
